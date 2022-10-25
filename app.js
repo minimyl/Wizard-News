e1aa10ce9e4709d3e95c7276816bf620279fe2a0
Andrew <root@minimyl.localdomain>
2022-10-24 20:43:23 -0400

    Added single posts and linked to it

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.get("/", (req, res) => {
       ${posts.map(post => `
         <div class='news-item'>
           <p>
-            <span class="news-position">${post.id}. ▲</span>${post.title}
+            <span class="news-position">${post.id}. ▲</span><a href="/posts/${post.id}"> ${post.title}</a>
             <small>(by ${post.name})</small>
           </p>
           <small class="news-info">
@@ -30,6 +30,39 @@ app.get("/", (req, res) => {
           </small>
         </div>`
       ).join('')}
+    </div>
+  </body>
+</html>`
+
+  res.send(html);
+});
+
+app.get('/posts/:id', (req, res) => {
+  const id = req.params.id;
+  // const posts = postBank.list()
+  const post = postBank.find(id);
+
+  const html = `<!DOCTYPE html>
+    <html>
+    <head>
+      <title>Wizard News</title>
+      <link rel="stylesheet" href="/style.css" />
+      
+    </head>
+    <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>${post.title}
+            <small>(by ${post.name})</small>
+            <div>${post.content}</div>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>
     </div>
   </body>
 </html>`
